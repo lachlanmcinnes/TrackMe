f234163a7f985fceb7c74ca263344717668eec57
Lachlan Mcinnes <lachlan.mcinnes@deswik.com>
2018-08-06 12:25:17 +1000

    NEW: add mqtt

---
+++ mqtt/mqtt.js
@@ -0,0 +1,34 @@
+const mqtt = require('mqtt');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const { URL, USERNAME, PASSWORD } = process.env;
+const port = process.env.PORT || 5001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const client = mqtt.connect(URL, {
+    username: USERNAME,
+    password: PASSWORD
+});
+
+client.on('connect', () => {
+    console.log('mqtt connected');
+});
+
+app.post('/send-command', (req,res) => {
+    const {deviceId, command} = req.body;
+    const topic = `/command/${deviceId}`;
+    client.publish(topic, command, () => {
+        res.send('published new message');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
